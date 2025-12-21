b8bbe3dc9353a9bbb64636d90646613792cb3adc
Takhir <guvalakat1603@gmail.com>
2025-12-21 10:37:40 +0500

    advanced configuration

--- src/main/java/uz/pdp/Main.java
+++ src/main/java/uz/pdp/Main.java
@@ -10,12 +10,14 @@ public class Main {
                 new ClassPathXmlApplicationContext("ioc-settings.xml");
 //        MyBean myBean = context.getBean(MyBean.class);
 //        myBean.hi();
-        MyBean2 myBean2 = context.getBean(MyBean2.class);
-        myBean2.getMyBean().hi();
-        Person person = context.getBean(Person.class);
-        System.out.println(person);
-        BaseDAO baseDAO = context.getBean(BaseDAO.class);
-        System.out.println(baseDAO);
+//        MyBean2 myBean2 = context.getBean(MyBean2.class);
+//        myBean2.getMyBean().hi();
+//        Person person = context.getBean(Person.class);
+//        System.out.println(person);
+//        BaseDAO baseDAO = context.getBean(BaseDAO.class);
+//        System.out.println(baseDAO);
+        Service service = context.getBean("userService",Service.class);
+        System.out.println("service: "+service);
         context.close();
     }
 }
---
+++ src/main/java/uz/pdp/RoleService.java
@@ -0,0 +1,4 @@
+package uz.pdp;
+
+public class RoleService implements Service {
+}
