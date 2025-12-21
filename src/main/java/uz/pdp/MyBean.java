2f70853b465074196674099e70be1ae3915acc8a
Takhir <guvalakat1603@gmail.com>
2025-12-21 10:44:16 +0500

    advanced configuration

--- src/main/java/uz/pdp/MyBean.java
+++ src/main/java/uz/pdp/MyBean.java
@@ -1,5 +1,7 @@
 package uz.pdp;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -7,5 +9,15 @@ public class MyBean {
 
     public void hi(){
         System.out.println("===== MyBean hi method worked. =====");
+    }
+
+    @PostConstruct
+    private void init(){
+        System.out.println("MyBean.... initialized.");
+    }
+
+    @PreDestroy
+    private void destroy(){
+        System.out.println("MyBean.... destroyed.");
     }
 }
