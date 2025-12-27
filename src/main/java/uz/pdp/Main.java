2254b026948d3b7cb56ab595875d4dfd0a6df842
Takhir <guvalakat1603@gmail.com>
2025-12-27 16:37:34 +0500

    spring aop

--- src/main/java/uz/pdp/Main.java
+++ src/main/java/uz/pdp/Main.java
@@ -1,19 +1,7 @@
 package uz.pdp;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.expression.Expression;
-import org.springframework.expression.ExpressionParser;
-import org.springframework.expression.spel.standard.SpelExpressionParser;
-
 public class Main {
     public static void main(String[] args) {
-        ExpressionParser parser = new SpelExpressionParser();
-        Expression expression = parser.parseExpression("('Pdp ' + 'Academy').toUpperCase()");
-        Object value = expression.getValue();
-        System.out.println(value);
 
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
-        User user = context.getBean(User.class);
-        System.out.println(user);
     }
 }
