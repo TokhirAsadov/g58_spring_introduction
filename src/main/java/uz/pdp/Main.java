c00bde6b23d8902015bfba4f43c18626953b8193
Takhir <guvalakat1603@gmail.com>
2026-01-11 12:19:25 +0500

    UserDAO2

--- src/main/java/uz/pdp/Main.java
+++ src/main/java/uz/pdp/Main.java
@@ -3,6 +3,7 @@ package uz.pdp;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import uz.pdp.user.User;
 import uz.pdp.user.UserDAO;
+import uz.pdp.user.UserDAO2;
 
 import java.util.List;
 
@@ -10,11 +11,12 @@ public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ioc_settings.xml");
         UserDAO userDAO = context.getBean(UserDAO.class);
+        UserDAO2 userDAO2 = context.getBean(UserDAO2.class);
         User user = User.builder()
-//                .id(3)
-                .username("Elmurod")
-                .password("666")
-                .age(38)
+//                .id(7)
+                .username("Umar")
+                .password("989")
+                .age(14)
                 .build();
 //        userDAO.save(user);
         /*User userDAOById = userDAO.findById(1);
@@ -23,7 +25,17 @@ public class Main {
         users.forEach(System.out::println);*/
 //        userDAO.update(user);
 //        userDAO.delete(3);
-        Integer id = userDAO.save2(user);
-        System.out.println("save2 -> id: "+id);
+//        Integer id = userDAO.save2(user);
+//        System.out.println("save2 -> id: "+id);
+
+//        Integer id = userDAO2.save2(user);
+//        System.out.println("userDAO2.save2 -> id: "+id);
+//        userDAO2.save(user);
+        /*User userDAO2ById = userDAO2.findById(7);
+        System.out.println(userDAO2ById);*/
+//        userDAO2.findAll().forEach(System.out::println);
+//        userDAO2.findAll(18, 40).forEach(System.out::println);
+        userDAO2.delete(7);
+
     }
 }
