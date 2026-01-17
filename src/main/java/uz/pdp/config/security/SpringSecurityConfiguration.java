b1e9cc1e66eb69ddb9b88c06e56e26516f8f1c62
Takhir <guvalakat1603@gmail.com>
2026-01-17 17:38:18 +0500

    spring security lesson 1.

--- src/main/java/uz/pdp/config/security/SpringSecurityConfiguration.java
+++ src/main/java/uz/pdp/config/security/SpringSecurityConfiguration.java
@@ -4,7 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -14,13 +19,45 @@ public class SpringSecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
+                .csrf(csrf -> csrf.disable()) // CSRF ni o'chirish (faqat test uchun)
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/home", "/homeModel").permitAll()
+                        .requestMatchers(
+                                "/auth/login", // auth ichidagi barcha api larni ruxsat berish
+                                "/homeModel",
+                                "/css/**",
+                                "/js/**"
+                        ).permitAll()
 
                         .anyRequest().authenticated() // barcha api larni autentifikatsiyadan otqazish kerak
                 );
 
+        http.formLogin()
+                .loginPage("/auth/login")
+                .usernameParameter("uname")
+                .passwordParameter("pswd")
+                .defaultSuccessUrl("/homeModel", false); // true bulsa har doim shu
 
+        http.logout()
+                .logoutUrl("/auth/logout")
+                .deleteCookies("JSESSIONID")
+                .clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout", "POST"))
+        ;
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withDefaultPasswordEncoder() // faqat test uchun ishlatilsin
+                .username("user")
+                .password("123")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("123")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
--- src/main/java/uz/pdp/controller/AuthController.java
+++ src/main/java/uz/pdp/controller/AuthController.java
@@ -9,9 +9,21 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/auth")
 public class AuthController {
 
+    // /auth/login
     @GetMapping("/login")
     public ModelAndView loginPage() {
         return new ModelAndView("auth/login");
     }
 
+    // /auth/login
+    @GetMapping("/logout")
+    public ModelAndView logoutPage() {
+        return new ModelAndView("auth/logout");
+    }
+
+    // /auth/register
+//    @GetMapping("/register")
+//    public ModelAndView registerPage() {
+//        return new ModelAndView("auth/register");
+//    }
 }
