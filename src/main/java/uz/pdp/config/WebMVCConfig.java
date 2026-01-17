cd2101b84b86ace13dbe495807835b4bc563c8ef
Takhir <guvalakat1603@gmail.com>
2026-01-17 16:00:51 +0500

    addResourceHandlers

--- src/main/java/uz/pdp/config/WebMVCConfig.java
+++ src/main/java/uz/pdp/config/WebMVCConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
@@ -50,4 +51,11 @@ public class WebMVCConfig implements WebMvcConfigurer {
         return viewResolver;
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/*")
+                .addResourceLocations("classpath:/static/css");
+        registry.addResourceHandler("/js/*")
+                .addResourceLocations("classpath:/static/js");
+    }
 }
