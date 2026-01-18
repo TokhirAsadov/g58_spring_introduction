68f35b66aca1ecc77b290470170410e40db6937e
Takhir <guvalakat1603@gmail.com>
2026-01-18 12:36:20 +0500

    Enhance Spring Security configuration with custom user details service and failure handler

--- src/main/java/uz/pdp/config/security/SpringSecurityConfiguration.java
+++ src/main/java/uz/pdp/config/security/SpringSecurityConfiguration.java
@@ -7,6 +7,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -15,7 +18,11 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 public class SpringSecurityConfiguration {
 
+    private final CustomUserDetailsService customUserDetailsService;
+    private final CustomAuthenticatedFailerHandler customAuthenticatedFailerHandler;
+
     private final String[] PUBLIC_URLS = {
+            "/auth/register",
             "/auth/login",
             "/auth/logout",
             "/auth/homeModel",
@@ -23,14 +30,21 @@ public class SpringSecurityConfiguration {
             "/js/**"
     };
 
+    public SpringSecurityConfiguration(CustomUserDetailsService customUserDetailsService, CustomAuthenticatedFailerHandler customAuthenticatedFailerHandler) {
+        this.customUserDetailsService = customUserDetailsService;
+        this.customAuthenticatedFailerHandler = customAuthenticatedFailerHandler;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
                 .csrf(csrf -> csrf.disable()) // CSRF ni o'chirish (faqat test uchun)
+                .userDetailsService(customUserDetailsService)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(PUBLIC_URLS).permitAll()
-
+                        .requestMatchers("/admin/**").hasRole("ADMIN") // faqat adminlarga ruxsat
+                        .requestMatchers("/user/**").hasAnyRole("USER","ADMIN") // faqat user lar uchun ruxsat
                         .anyRequest().authenticated() // barcha api larni autentifikatsiyadan otqazish kerak
                 );
 
@@ -38,7 +52,9 @@ public class SpringSecurityConfiguration {
                 .loginPage("/auth/login")
                 .usernameParameter("uname")
                 .passwordParameter("pswd")
-                .defaultSuccessUrl("/homeModel", false); // true bulsa har doim shu
+                .defaultSuccessUrl("/homeModel", false)
+                .failureHandler(customAuthenticatedFailerHandler)
+        ; // true bulsa har doim shu
 
         http.logout()
                 .logoutUrl("/auth/logout")
@@ -46,10 +62,23 @@ public class SpringSecurityConfiguration {
                 .clearAuthentication(true)
                 .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout", "POST"))
         ;
+
+        http.rememberMe()
+                .rememberMeParameter("rememberMe")
+                .rememberMeCookieName("remmeber-me-cookie")
+                .tokenValiditySeconds(24 * 60 * 60)
+                .key("secret_key")
+                .userDetailsService(customUserDetailsService);
         return http.build();
     }
 
     @Bean
+    public PasswordEncoder passwordEncoder() {
+//        return NoOpPasswordEncoder.getInstance(); // faqat test uchun ishlatilsin
+        return new BCryptPasswordEncoder();
+    }
+
+   /* @Bean
     public UserDetailsService userDetailsService(){
         UserDetails user = User.withDefaultPasswordEncoder() // faqat test uchun ishlatilsin
                 .username("user")
@@ -62,5 +91,5 @@ public class SpringSecurityConfiguration {
                 .roles("ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(user, admin);
-    }
+    }*/
 }
