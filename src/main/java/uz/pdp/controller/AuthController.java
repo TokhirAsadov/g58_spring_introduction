08503dfb49a0a36ff47b7750df96c07b131117c7
Takhir <guvalakat1603@gmail.com>
2026-01-18 12:38:00 +0500

    Add user registration functionality to AuthController

--- src/main/java/uz/pdp/controller/AuthController.java
+++ src/main/java/uz/pdp/controller/AuthController.java
@@ -1,18 +1,30 @@
 package uz.pdp.controller;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import uz.pdp.dao.AuthUserDao;
+import uz.pdp.entity.AuthUser;
 
 @Controller
 @RequestMapping("/auth")
 public class AuthController {
 
+    private final AuthUserDao authUserDao;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthController(AuthUserDao authUserDao, PasswordEncoder passwordEncoder) {
+        this.authUserDao = authUserDao;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     // /auth/login
     @GetMapping("/login")
-    public ModelAndView loginPage() {
-        return new ModelAndView("auth/login");
+    public ModelAndView loginPage(@RequestParam(value = "error",required = false) String error) {
+        ModelAndView modelAndView = new ModelAndView("auth/login");
+        modelAndView.addObject("errorMessage", error);
+        return modelAndView;
     }
 
     // /auth/login
@@ -22,8 +34,20 @@ public class AuthController {
     }
 
     // /auth/register
-//    @GetMapping("/register")
-//    public ModelAndView registerPage() {
-//        return new ModelAndView("auth/register");
-//    }
+    @GetMapping("/register")
+    public ModelAndView registerPage() {
+        return new ModelAndView("auth/register");
+    }
+
+    @PostMapping("/register")
+    public ModelAndView registerUser(@ModelAttribute AuthUser authUser) {
+        System.out.println("Registering user: " + authUser);
+        Integer id = authUserDao.save(AuthUser.builder()
+                .username(authUser.getUsername())
+                .password(passwordEncoder.encode(authUser.getPassword()))
+                .role("USER")
+                .build());
+        System.out.println("Saved user id: " + id);
+        return new ModelAndView("redirect:/auth/login");
+    }
 }
