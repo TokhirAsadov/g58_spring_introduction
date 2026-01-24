c10beb4af07603b0ab9ffcae5eeb9484178d9b39
Takhir <guvalakat1603@gmail.com>
2026-01-24 16:50:05 +0500

    add security annotations to AdminController for role-based access control

--- src/main/java/uz/pdp/controller/AdminController.java
+++ src/main/java/uz/pdp/controller/AdminController.java
@@ -1,5 +1,7 @@
 package uz.pdp.controller;
 
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ public class AdminController {
 
     @GetMapping("/dashboard")
     @ResponseBody
+    @PreAuthorize("hasRole('ADMIN')")
     public String dashboard() {
         return "Admin Dashboard";
     }
