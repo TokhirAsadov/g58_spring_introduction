fdd94bdca2628da5ad854b6a8d0d9eddef67a39f
Takhir <guvalakat1603@gmail.com>
2026-01-31 16:44:29 +0500

    refactor HomeController to support language preference from cookies and request parameters

--- src/main/java/uz/pdp/controller/HomeController.java
+++ src/main/java/uz/pdp/controller/HomeController.java
@@ -2,10 +2,12 @@ package uz.pdp.controller;
 
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Locale;
+import java.util.Objects;
 
 @Controller
 public class HomeController {
@@ -16,22 +18,23 @@ public class HomeController {
     }
 
     @GetMapping("/home")
-    public String home(@RequestParam(name = "lang", required = false) String lang) {
+    public String home(@CookieValue(name = "language",required = false) String cookieLang, @RequestParam(name = "lang", required = false) String language) {
+        String lang = Objects.requireNonNullElse(language, cookieLang != null ? cookieLang : "uz");
         String message = messageSource.getMessage(
                 "welcome",
                 null,
-                Locale.forLanguageTag(lang != null ? lang : "uz")
+                Locale.forLanguageTag(lang)
         );
 
         String message2 = messageSource.getMessage(
                 "welcome2",
                 new Object[]{"Umar"},
-                Locale.forLanguageTag(lang != null ? lang : "uz")
+                Locale.forLanguageTag(lang)
         );
         String message3 = messageSource.getMessage(
                 "welcome3",
-                new Object[]{"Umar","Usmon","Ali"},
-                Locale.forLanguageTag(lang != null ? lang : "uz")
+                new Object[]{"Umar", "Usmon", "Ali"},
+                Locale.forLanguageTag(lang)
         );
         System.out.println("Message: " + message);
         System.out.println("Message2: " + message2);
